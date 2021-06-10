9b2703886062e442aef62b2f52bd580d6be672f9
NAME <mail@gmail.com>
2021-06-10 07:50:33 -0300

    Criacao de condicao para evitar cadastro de dependentes duplicados TESTE

--- src/Scripts/Manipulations/Admin/Resp/registrarDependentes.php
+++ src/Scripts/Manipulations/Admin/Resp/registrarDependentes.php
@@ -16,8 +16,14 @@ foreach($alunos as $aluno)
 {
 
 
+$check = "SELECT * from relacao_alunosresponsaveis WHERE RM_Aluno = '$aluno' AND Responsavel_Filhos='$rm';";
+$checkQuery = mysqli_query($conn, $check);
+$checkRow = mysqli_fetch_row($checkQuery);
 
-
+if(!($checkRow==0))
+{
+	throw new Exception();
+	}
 $query .= "INSERT into relacao_alunosresponsaveis (RM_Aluno, Responsavel_Filhos) VALUES ( '$aluno', '$rm');";
  
 
